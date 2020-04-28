a12a845626b92958d9f837a141efcff2eb953ec2
Byron Hall <byronhall@MacBook-Pro.local>
2020-04-28 11:03:42 -0500

    added lorem ipsum, TODO: test swipe functionality

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-5">
+    <div className="text-center">&quot;I can no longer obey; I have tasted command, and I cannot give it up.&quot;</div>
+    <div className="text-center">a Byron J-Mil Hall production</div>
+  </div>
+);
+
+export default Footer;

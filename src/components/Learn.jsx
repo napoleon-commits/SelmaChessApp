4a88eeab38d4651e1771857f98194b8369385b83
Byron Hall <byronhall@MacBook-Pro.local>
2020-04-29 15:02:13 -0500

    spelling
    special moves
    
    added img and gif
    
    freemode skeleton
    
    chess engine
    
    engine TODO: s3 bucket the images
    
    s3 bucket
    
    added css
    
    take back and new game

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -8,6 +8,9 @@ import queenMovements from '../images/QueenMovements.png';
 import kingMovements from '../images/KingMovements.png';
 import pawnMovements from '../images/PawnMovements.png';
 
+import check from '../images/check.png';
+import castling from '../images/castling.gif';
+
 const Learn = () => (
   <div className="px-3">
     <div className="h1">Rules of chess</div>
@@ -19,6 +22,7 @@ const Learn = () => (
         When a king cannot avoid capture then it is checkmated and the game is over.
       </div>
     </div>
+    <hr />
     <div className="mb-3">
       <div className="h1 text-secondary">Starting position</div>
       <div className="mb-3">
@@ -30,8 +34,9 @@ const Learn = () => (
         <img src={startingPosition} alt="starting chess position" style={{ width: '100%' }} />
       </div>
     </div>
+    <hr />
     <div className="mb-3">
-      <div className="h1 text-secondary">Moving the chess pieces</div>
+      <div className="h1 text-secondary">Moving the pieces</div>
     </div>
     <div className="mb-3">
       <div className="h3 text-white">Knight</div>
@@ -69,7 +74,7 @@ const Learn = () => (
     <div className="mb-3">
       <div className="h3 text-white">King</div>
       <div className="mb-3">
-        The king can move one square in any direction. The king also has
+        The king can move one square in any direction. The king has
         a special move called castling.
       </div>
       <div>
@@ -99,8 +104,56 @@ const Learn = () => (
         <img src={pawnMovements} alt="Pawn Movements" style={{ width: '100%' }} />
       </div>
     </div>
+    <hr />
     <div className="mb-3">
-      <div className="h2 text-secondary">Special Moves</div>
+      <div className="h1 text-secondary">Special Moves</div>
+    </div>
+    <div className="mb-3">
+      <div className="h3 text-white">Check</div>
+      <div className="mb-3">
+        When a king is under immediate attack by an opponent&apos;s piece,
+        it is said to be in check.
+        The only option is to remove the king from check.
+        This can be capturing the attacking piece, placing a piece
+        in between the attacker and the king, or move the king to a
+        square where it is no longer in check. The king can not castle when it is in check.
+      </div>
+      <div>
+        <img src={check} alt="Check" style={{ width: '100%' }} />
+      </div>
+    </div>
+    <div className="mb-3">
+      <div className="h3 text-white">Castling</div>
+      <div className="mb-3">
+        Castling is a move often made in the early parts of the game.
+        In most cases, it moves the king to a safer position.
+        Castling involves the moving of the king two squares to the left or
+        two squares to the right, and placing the rook to the opposite side of the king.
+        A player is only allowed to castle if the king and rook have not been moved.
+        Also, castling can not happen if the king is in check,
+        or if the king will pass through a check,
+        or if the king will end up in check.
+      </div>
+      <div>
+        <img src={castling} alt="Castle Movement" style={{ width: '100%' }} />
+      </div>
+    </div>
+    <div className="mb-3">
+      <div className="h3 text-white">Pawn Promoting</div>
+      <div className="mb-3">
+        When a pawn reaches the end of the board, that pawn can be promoted.
+        Then the pawn can be exchanged for a queen, rook, bishop, or knight.
+        Usually, the pawn is promoted to a queen. In some strategies the pawn is
+        promoted to a rook, bishop, or knight. This is called underpromiting.
+      </div>
+    </div>
+    <div className="mb-3">
+      <div className="h3 text-white">En passant</div>
+      <div className="mb-3">
+        When a pawn moves forward two squares and there is an opponent&apos;s pawn next to it,
+        the opponent can capture the pawn by moving to the square the first pawn skipped.
+        This can be done only on the very next turn.
+      </div>
     </div>
   </div>
 );

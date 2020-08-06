9f474deb4d7b4b56da0dbe58683fe747f5cd9790
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-08-06 17:19:49 -0500

    only click time pairing once

--- src/components/PlayOnline.jsx
+++ src/components/PlayOnline.jsx
@@ -19,12 +19,22 @@ class PlayOnline extends React.Component {
       yourTurn: false,
       foundOpponent: false,
       playerSide: null,
+      initConnection: false,
     };
+    this.createWebSocket = this.createWebSocket.bind(this);
     this.initializeWebSocket = this.initializeWebSocket.bind(this);
     this.updateBoard = this.updateBoard.bind(this);
   }
 
   initializeWebSocket() {
+    const { initConnection } = this.state;
+    if (!initConnection) {
+      this.setState({ initConnection: true });
+      this.createWebSocket();
+    }
+  }
+
+  createWebSocket() {
     this.ws = new WebSocket('wss://as4r4pfx8l.execute-api.us-east-1.amazonaws.com/dev');
     this.ws.onopen = () => {
       this.setState({
@@ -111,40 +121,40 @@ class PlayOnline extends React.Component {
         {
           foundOpponent && madeConnection
             ? (
-                <>
-                  <LiveBoard
-                    boardArray={
+              <>
+                <LiveBoard
+                  boardArray={
                         playerSide === 0
                           ? boardArray
                           : reversedBoardArray
                     }
-                    webSocket={webSocket}
-                    yourTurn={yourTurn}
-                    playerSide={playerSide}
-                    updateBoard={this.updateBoard}
-                  />
-                </>
-              )
+                  webSocket={webSocket}
+                  yourTurn={yourTurn}
+                  playerSide={playerSide}
+                  updateBoard={this.updateBoard}
+                />
+              </>
+            )
             : (
-                <div className="mx-3">
-                  <div className="w-75 m-auto h1 text-center">Choose Your Time Limit</div>
-                  <div className="w-50 mx-auto mt-4">
-                    <div
-                      className="text-center game-select"
-                      onClick={this.initializeWebSocket}
-                      onKeyDown={this.initializeWebSocket}
-                    >
-                      <div className="mt-4">30+0</div>
-                      <div>Classic</div>
-                      {
+              <div className="mx-3">
+                <div className="w-75 m-auto h1 text-center">Choose Your Time Limit</div>
+                <div className="w-50 mx-auto mt-4">
+                  <div
+                    className="text-center game-select"
+                    onClick={this.initializeWebSocket}
+                    onKeyDown={this.initializeWebSocket}
+                  >
+                    <div className="mt-4">30+0</div>
+                    <div>Classic</div>
+                    {
                             madeConnection && !foundOpponent
                               ? <div className="spinner-border text-secondary" role="status" />
                               : <div className="spinner-border text-tertiary" role="status" />
                         }
-                    </div>
                   </div>
                 </div>
-              )
+              </div>
+            )
         }
         <div className="px-3"><Footer /></div>
       </div>

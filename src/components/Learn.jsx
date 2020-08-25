99cb589abb4fa3e0879cd6ed3870f53639f88cc4
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-08-25 18:07:11 -0500

    button collapse

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -12,53 +12,69 @@ const Learn = () => (
   <div className="bg-primary text-white">
     <CustomNav />
     <div className="px-3">
-      <div className="h1">Rules of chess</div>
-      <hr />
       <div className="mb-3">
-        <div className="h1 text-secondary">What is the goal of a chess game?</div>
-        <div>
-          The goal in chess is to checkmate your opponent&apos;s King.
-          When a king cannot avoid capture then it is checkmated and the game is over.
+        <div className="h2">Getting Started</div>
+      </div>
+      <div className="mb-3">
+        <button className="btn btn-secondary w-100 text-left mb-2" type="button" data-toggle="collapse" data-target="#GoalOfChess" aria-expanded="false" aria-controls="GoalOfChess">
+          What is the goal of a chess game?
+        </button>
+        <div className="collapse" id="GoalOfChess">
+          <div className="mb-3">
+            The goal in chess is to checkmate your opponent&apos;s King.
+            When a king cannot avoid capture, then it is checkmated and the game is over.
+          </div>
+        </div>
+      </div>
+      <div className="mb-3">
+        <button className="btn btn-secondary w-100 text-left mb-2" type="button" data-toggle="collapse" data-target="#StartingPosition" aria-expanded="false" aria-controls="StartingPosition">
+          Starting position
+        </button>
+        <div className="collapse" id="StartingPosition">
+          <div className="mb-3">
+            The chessboard is placed with the light square at the right-hand
+            corner nearest to each player.
+            The rows are referred to as ranks. The columns are referred to as files.
+          </div>
+          <div>
+            <img src={startingPosition} alt="starting chess position" className="d-block mx-auto" style={{ maxWidth: '100%', height: '60vh' }} />
+          </div>
         </div>
       </div>
       <hr />
       <div className="mb-3">
-        <div className="h1 text-secondary">Starting position</div>
-        <div className="mb-3">
-          The chessboard is placed with the light square at the right-hand
-          corner nearest to each player.
-          The rows are referred to as ranks. The columns are referred to as files.
-        </div>
-        <div>
-          <img src={startingPosition} alt="starting chess position" className="d-block mx-auto" style={{ maxWidth: '100%', height: '60vh' }} />
-        </div>
-      </div>
-      <hr />
-      <div className="mb-3">
-        <div className="h1 text-secondary">Moving the pieces</div>
+        <div className="h2">Moving the pieces</div>
       </div>
       {
         ChessMovements.map((obj) => (
           <div key={obj.piece} className="mb-3">
-            <div className="h3 text-white">{obj.piece}</div>
-            <div className="mb-3">{obj.rules}</div>
-            <div>
-              <img src={obj.image} alt={obj.alt} className="d-block mx-auto" style={{ maxWidth: '100%', height: '60vh' }} />
+            <button className="btn btn-secondary w-100 text-left mb-2" type="button" data-toggle="collapse" data-target={`#${String(obj.piece).replace(' ', '')}`} aria-expanded="false" aria-controls={obj.piece}>
+              {obj.piece}
+            </button>
+            <div className="collapse" id={String(obj.piece).replace(' ', '')}>
+              <div className="mb-3">{obj.rules}</div>
+              <div>
+                <img src={obj.image} alt={obj.alt} className="d-block mx-auto" style={{ maxWidth: '100%', height: '60vh' }} />
+              </div>
             </div>
           </div>
         ))
       }
-      <hr />
+      {/* <hr /> */}
       <div className="mb-3">
-        <div className="h1 text-secondary">Special Moves</div>
+        <div className="h2">Special Moves</div>
       </div>
       {
         SpecialMoves.map((obj) => (
           <div key={obj.move} className="mb-3">
-            <div className="h3 text-white">{obj.move}</div>
-            <div className="mb-3">{obj.description}</div>
-            <div>
-              <img src={obj.image} alt={obj.alt} className="d-block mx-auto" style={{ maxWidth: '100%', height: '60vh' }} />
+            <button className="btn btn-secondary w-100 text-left mb-2" type="button" data-toggle="collapse" data-target={`#${String(obj.move).replace(' ', '')}`} aria-expanded="false" aria-controls={obj.move}>
+              {obj.move}
+            </button>
+            <div className="collapse" id={String(obj.move).replace(' ', '')}>
+              <div className="mb-3">{obj.description}</div>
+              <div>
+                <img src={obj.image} alt={obj.alt} className="d-block mx-auto" style={{ maxWidth: '100%', height: '60vh' }} />
+              </div>
             </div>
           </div>
         ))

a5f9f6cfc04b463b086eccdc3217ca3984ad6325
Byron Hall <bjhall2@crimson.ua.edu>
2020-09-13 16:36:54 -0500

    square

---
+++ src/utils/square.vue
@@ -0,0 +1,18 @@
+<template>
+    <img
+      :className="className"
+      :alt="letter"
+      :style="imagesStyleObject"
+      :src="imageSrc"
+    />
+</template>
+
+<script>
+export default {
+    props: ['className', 'letter', 'imagesStyleObject', 'imageSrc',]
+}
+</script>
+
+<style>
+
+</style>

286c2eca164cc6b7de61097c74e897c03538c382
Byron Hall <bjhall2@crimson.ua.edu>
2020-09-14 14:04:03 -0500

    toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <Toolbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Toolbar from './components/Toolbar';
+
 export default {
   name: 'App',
+  components:{
+    Toolbar
+  }
 };
 </script>
 
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="toolbar">Toolbar</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #toolbar{
+         box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2);
+    }
+</style>

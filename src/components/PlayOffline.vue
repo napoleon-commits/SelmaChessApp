43a3352ca74410f0d659c6beecdcf063f3feb417
Byron Hall <bjhall2@crimson.ua.edu>
2020-09-13 21:01:40 -0500

    functioning game

--- src/components/PlayOffline.vue
+++ src/components/PlayOffline.vue
@@ -1,13 +1,25 @@
 <template>
-<div class="mt-2">
+<div class="mx-4 mt-2">
   <table class="m-auto">
       <tbody>
           <tr :key="rankIndex" v-for="(rank, rankIndex) in chessboard">
               <td
-                :key="squareIndex"
-                v-for="(square, squareIndex) in rank"
+                :key="fileIndex"
+                v-for="(square, fileIndex) in rank"
                 v-html="getHTMLChessPiece(square)"
-                @click="squareClick"
+                @click="squareClick(
+                  8-rankIndex,
+                  1+fileIndex,
+                  `${(chessboard[rankIndex][fileIndex] !== '.')?'Piece':'Square'}`
+                )"
+                :class="`
+                  ${(((rankIndex+fileIndex)%2)===0)?'bg-white':'dark-square'}
+                  ${(
+                    rankIndex === rankSelected
+                    && fileIndex === fileSelected
+                  )?' square-selected':''}
+                  ${(chessboard[rankIndex][fileIndex] !== '.')?' c-pointer':''}
+                  `"
                 />
           </tr>
       </tbody>
@@ -18,23 +30,48 @@
 <script>
 import { startBoard } from '../constants';
 import getHTMLChessPiece from '../utils/board';
+import { clickedPieceJSX, clickedSquareJSX, getJSXBoard, newGame } from '../utils/engine';
 
 export default {
   data() {
     return {
       chessboard: startBoard,
+      rankSelected: null,
+      fileSelected: null,
     };
   },
   methods: {
     getHTMLChessPiece,
-    squareClick() {
-      // eslint-disable-next-line
-      console.log('!!!');
+    squareClick(rank, file, type) {
+      if (type === 'Piece') {
+        clickedPieceJSX(file, rank);
+      } else if (type === 'Square') {
+        clickedSquareJSX(file, rank);
+      }
+      this.chessboard = getJSXBoard();
+      if (this.fileSelected !== null || this.rankSelected !== null || type === 'Square') {
+        this.rankSelected = null;
+        this.fileSelected = null;
+      } else {
+        this.rankSelected = 8 - rank;
+        this.fileSelected = file - 1;
+      }
     },
+  },
+  mounted() {
+    newGame();
   },
 };
 </script>
 
 <style>
-
+  .dark-square{
+    background-color: #61dafb;
+  }
+  .square-selected{
+    background-color: #6d7a82 !important;
+  }
+  .c-pointer{
+    cursor: pointer;
+  }
 </style>

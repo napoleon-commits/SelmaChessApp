76e4234f63c477e87d57c7e6d3134ec5db151b9e
Byron Hall <bjhall2@crimson.ua.edu>
2020-10-06 16:17:56 -0500

    custom toggle button for auto rotate

--- src/components/Engine.vue
+++ src/components/Engine.vue
@@ -44,6 +44,13 @@
         <option value="8">8s</option>
         <option value="10">10s</option>
         </select>
+        <br /><br />
+        <div>
+          <label class="switch">
+            <input type="checkbox" v-model="autoRotate">
+            <span class="slider round"></span>
+          </label>
+        </div>
       </div>
       <div class="col">
         <span>BestMove: {{BestMove}}</span><br />
@@ -102,6 +109,7 @@ export default {
       chessboardSize: null,
       displayModal: false,
       firstClick: false,
+      autoRotate: true,
     };
   },
   mounted() {
@@ -220,4 +228,56 @@ export default {
   .square-selected{
     background-color: #6d7a82 !important;
   }
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  input:checked + .slider {
+    background-color: #42b983;
+  }
+  input:focus + .slider {
+    box-shadow: 0 0 1px #42b983;
+  }
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+  .slider.round:before {
+    border-radius: 50%;
+  }
+
 </style>

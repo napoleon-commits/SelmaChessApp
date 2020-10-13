314ad865f269870fcb0f63fd4282dca7e63aa67e
Byron Hall <bjhall2@crimson.ua.edu>
2020-10-12 21:30:43 -0500

    hanging css

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -2,7 +2,7 @@
   <div id="toolbar">
     <v-toolbar
       :class="this.$store.state.darkModeClass"
-      style="box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2) !important; border-top:"
+      style="box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2) !important;"
     >
       <div class="dropdown">
         <button

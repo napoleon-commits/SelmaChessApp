48937a290955b4a9ed19903de193bf70c3da7f11
Byron Hall <bjhall2@crimson.ua.edu>
2020-10-14 19:00:33 -0500

    placeholder css

--- src/components/Engine.vue
+++ src/components/Engine.vue
@@ -3,8 +3,16 @@
     <div :style="{width: `${chessboardSize+8}px`}" class="row mx-auto">
       <div class="col text-center">
           <span>Fen: </span>
-          <input :style="`border: 2px solid ${customColor}`" type="text" v-model="fenIn"/>
-          <button @click="vueSetFen">Set Position</button>
+          <input
+            :style="`border: 2px solid ${customColor}`"
+            type="text"
+            v-model="fenIn"
+            class="w-50"
+            placeholder="7k/p7/1R5K/6r1/6p1/6P1/8/8 w - - bm Rb7"
+          />
+          <button @click="vueSetFen" :style="`border: 2px solid ${customColor}`">
+            Set Position
+          </button>
       </div>
     </div>
     <div>

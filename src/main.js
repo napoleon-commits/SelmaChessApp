8200c012e0ec654202cd2bf079afd969b5d572a1
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-12-06 16:04:54 -0600

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import vuetify from '@/plugins/vuetify'; // path to vuetify export
 import App from './App';
 import router from './router';
 import store from './store';
@@ -13,5 +14,6 @@ new Vue({
   router,
   components: { App },
   store,
+  vuetify,
   template: '<App/>',
 });

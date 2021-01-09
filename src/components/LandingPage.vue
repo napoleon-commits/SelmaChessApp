f30fc42690e494fac32d98e0317f23c7ca5d345a
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-09 10:36:26 -0600

    updated landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,11 +1,36 @@
 <template>
-  <v-container>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in finibus orci. Sed condimentum mi eu porta scelerisque. Donec ac dui pharetra, vestibulum lorem vel, sollicitudin arcu. In eu lacinia elit. Nulla rutrum consequat lacinia. Aenean commodo nibh non orci semper, eu viverra augue vehicula. Mauris auctor cursus elit ac commodo. Ut a ultrices dui, id aliquam lacus.
-    Suspendisse vitae leo eget nisi fringilla lobortis. Nulla sed accumsan libero. Mauris vitae nunc sodales, consectetur nunc eu, volutpat ante. Etiam et lacinia lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam molestie non ex eu maximus. Fusce condimentum pharetra commodo. Curabitur ut massa pretium, auctor nibh in, semper erat.
-    Fusce lacinia erat id efficitur mattis. Nulla dictum accumsan viverra. Pellentesque posuere porta eros, mollis ultricies ligula laoreet vel. Suspendisse euismod nunc et velit pretium bibendum. Fusce commodo auctor nisi, sed tincidunt sapien tempor vel. Pellentesque in ornare risus. Aenean tempor sed magna tincidunt aliquet. Pellentesque et enim eleifend orci tempor auctor. Duis sed maximus enim, in interdum turpis. Vivamus arcu elit, luctus non lorem ac, volutpat fermentum nisi. Aenean luctus dolor at bibendum ultricies.
-    Quisque ultrices semper neque ac convallis. Nulla eget turpis sit amet augue maximus placerat. Morbi porttitor sodales ipsum, sit amet consectetur erat pulvinar elementum. Vestibulum lacus sapien, ornare et tempus nec, fermentum a elit. Donec id laoreet sem, non bibendum metus. Nam quis risus nec elit dignissim dictum non eget elit. Mauris pellentesque elit nibh, et mattis velit imperdiet eu. Suspendisse potenti. Suspendisse ultricies lacus in libero euismod porttitor. Duis facilisis ultrices velit, vitae viverra erat egestas in. Aliquam eget mi vitae nisi ultrices tempus. In porta vehicula magna. Proin dapibus pretium diam, eu ultrices eros tristique ornare. Nulla mattis egestas quam, in malesuada sapien rhoncus convallis. Praesent eu sagittis neque, vel ornare mi. Fusce imperdiet erat id sapien sagittis, et ullamcorper erat dapibus.
-    Donec interdum arcu vel diam sagittis finibus. Donec pharetra justo ac elit lobortis laoreet. Integer et ligula imperdiet, scelerisque dolor at, luctus felis. Pellentesque auctor est lorem, ut tempus nibh iaculis a. Cras et eleifend odio. Sed at condimentum nisl, eget volutpat ipsum. Aliquam finibus tortor turpis, a rutrum est aliquet at. Fusce a elit sem. Nam massa lacus, pulvinar laoreet congue at, congue id erat. Nullam id imperdiet ipsum. Mauris eget lobortis purus. Duis id magna tempor, faucibus nisl dictum, accumsan arcu. Vestibulum iaculis justo commodo finibus placerat. In hac habitasse platea dictumst.
-  </v-container>
+  <div class="mt-3">
+    <div class="text-xl-h1 text-lg-h1 text-md-h2 text-sm-h3 text-h4 mb-1">
+      Introduction
+    </div>
+    <div class="text-body-1 mb-3">
+      Successfully lead an empire! Imagine soldiers on foot leading the charge.
+      Imagine the mounted troops on horseback. Imagine the King with the Queen at his side.
+      In many battles, when the King dies or is captured the battle ends.
+      And so it is in the game of chess.
+    </div>
+    <v-menu offset-y :close-on-content-click="false">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          block
+          :color="customColor"
+          outlined
+        >
+          <span :style="`color: ${textColor}`">What is the goal of a chess game?</span>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>
+            The goal in chess is to checkmate your opponent's King.
+            When a king cannot avoid capture, then it is checkmated and the game is over.
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script>
@@ -15,5 +40,17 @@
     data: () => ({
 
     }),
+    computed:{
+      customColor(){
+        const {red, green, blue} = this.$store.state.customColor;
+        return `rgb(${red},${green},${blue})`
+      },
+      textColor(){
+        if(this.$vuetify.theme.dark){
+          return '#FFF';
+        }
+        return 'rgba(0,0,0,0.87)';
+      }
+    }
   }
 </script>

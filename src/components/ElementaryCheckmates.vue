549b3dac7fc7cc9f3b045b9c003a8c24279acae3
Byron Hall <byron.hall@ricardodefense.com>
2021-01-19 11:21:38 -0800

    added layout for ElementaryCheckmates component

--- src/components/ElementaryCheckmates.vue
+++ src/components/ElementaryCheckmates.vue
@@ -10,19 +10,82 @@
             <div class="text-xl-h2 text-lg-h2 text-md-h3 text-sm-h4 text-h5 mb-3">
                 {{object.header}}
             </div>
+            <div class="text-body-1 my-3">
+                {{object.body}}
+            </div>
+            <div v-for="item in object.buttons" :key="item.buttonText">
+                <v-dialog>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            v-bind="attrs"
+                            v-on="on"
+                            block
+                            :class="btnClass"
+                            outlined
+                        >
+                            <span :style="`color: ${textColor}`">{{item.buttonText}}</span>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                    <v-list-item>
+                        <v-list-item-title class="text-wrap">
+                            <DynamicChessBoard
+                                :moves="item.moves"
+                                v-if="item.moves"
+                            />
+                        </v-list-item-title>
+                    </v-list-item>
+                    </v-list>
+                </v-dialog>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import ElementaryCheckmatesText from '@/static/ElementaryCheckmatesText';
+import DynamicChessBoard from '@/components/subcomponents/DynamicChessBoard';
 
 export default {
     name: 'ElementaryCheckmates',
+    components: {
+        DynamicChessBoard,
+    },
     data(){
         return {
             ElementaryCheckmatesText,
         };
+    },
+    computed: {
+        textColor(){
+            if(this.$vuetify.theme.dark){
+                return '#FFF';
+            }
+                return 'rgba(0,0,0,0.87)';
+        },
+        btnClass(){
+            const {red, green, blue} = this.$store.state.customColor;
+            if(red==127 && green==127 && blue==127){
+                return 'btn-default';
+            }
+            if(red==70 && green==165 && blue==81){
+                return 'btn-dallas-county';
+            }
+            if(red==155 && green==221 && blue==255){
+                return 'btn-selma';
+            }
+            if(red==255 && green==5 && blue==5){
+                return 'btn-southside';
+            }
+            if(red==0 && green==110 && blue==156){
+                return 'btn-keith';
+            }
+            return '';
+        },
+        customColor(){
+            const {red, green, blue} = this.$store.state.customColor;
+            return `rgb(${red},${green},${blue})`
+        },
     },
 }
 </script>
--- src/static/ElementaryCheckmatesText.js
+++ src/static/ElementaryCheckmatesText.js
@@ -11,24 +11,117 @@ export default {
     items: [
         {
             header: 'Two Queens',
+            body: `
+                You can have more than one Queen by promoting pawns. Some players enjoy promoting more than two,
+                if they can, but two Queens are almost always enough to deliver checkmate.
+            `,
+            buttons: [
+                {
+                    buttonText: `The Basic Checkmate`,
+                    moves: [
+                        '....................k...Q........Q..............................',
+                        '.................Q..k...Q.......................................',
+                        '...........k.....Q......Q.......................................',
+                        '........Q..k.....Q..............................................',
+                        '....k...Q........Q..............................................',
+                        '.Q..k...Q.......................................................',
+                    ]
+                },
+                {
+                    buttonText: `Another Way to Checkmate`,
+                    moves: [
+                        '.........................Q...........k....Q.....................',
+                        '............................Q........k....Q.....................',
+                        '............................Q.........k...Q.....................',
+                        '............................Q.........k.......Q.................',
+                    ]
+                },
+            ],
         },
         {
             header: 'Queen and Rook',
+            body: `
+                Together, the Queen and Rook are almost as powerful as the two Queens. And in most chess games,
+                you are likely to have a Queen and Rook in an endgame than two Queens.
+            `,
+            buttons: [
+                {
+                    buttonText: `The First Technique`,
+                },
+                {
+                    buttonText: `The Second Technique`,
+                },
+            ],
         },
         {
             header: 'Two Rooks',
+            body: `
+                Using two Rooks is another common checkmate.
+                It is slightly more difficult to do because the Rooks are not as powerful as the Queens.
+            `,
+            buttons: [
+                {
+                    buttonText: 'How to Get There',
+                }
+            ],
         },
         {
             header: 'King and Queen',
+            body: `
+                It might surprise you to learn that the Queen alone cannot deliver checkmate.
+                When it's down to King and Queen vs. the King,
+                the Queen requires close cooperation with its King to deliver the mate.
+            `,
+            buttons: [
+                {
+                    buttonText: 'A Simple Checkmate',
+                },
+                {
+                    buttonText: 'Other Ways to Checkmate',
+                },
+                {
+                    buttonText: 'Moves to Avoid',
+                },
+            ],
         },
         {
             header: 'King and Rook',
+            body: `
+                Mating with a Rook is somewhat more challenging than mating with a Queen,
+                although many players prefer this mate because there are fewer possibilities for a stalemate.
+            `,
+            buttons: [
+                {
+                    buttonText: 'The Basic Checkmate',
+                }
+            ]
         },
         {
             header: 'King and Two Bishops',
+            body: `
+                A checkmate with two Bishops does not occur often. If it does, you will fid that patience is required.
+                The first step is to force the King toward any edge of the board and then toward any corner.
+            `,
+            buttons: [
+                {
+                    buttonText: 'How to Get There'
+                }
+            ],
         },
         {
             header: 'King, Bishop, and Knight',
+            body: `
+                Of all the checkmates on this page, the checkmate with a King, Bishop, and Knight is the most complex.
+                It is so difficult
+                that some masters have failed to deliver the checkmate within the 50-move limit rule.
+                (The U.S. Chess Federation rule book states that the result of a game is a draw if,
+                after 50 consecutive moves by each player, there have been no captures or pawn moves.)
+            `,
+            buttons: [
+                {
+                    buttonText: 'How to Get There'
+                }
+            ]
         },
     ],
 }

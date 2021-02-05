3ebd2098b8d4bf20d24942f338a77e2bd14fee9b
Byron Hall <byronhall@MacBook-Pro.local>
2021-02-04 18:34:13 -0600

    added MiddlegameStrategy Lessons

--- src/components/MiddlegameStrategy.vue
+++ src/components/MiddlegameStrategy.vue
@@ -10,15 +10,28 @@
             <div class="text-xl-h2 text-lg-h2 text-md-h3 text-sm-h4 text-h5 mb-3">
                 {{object.header}}
             </div>
+            <div v-if="object.subtitle" class="mb-3 text-justify">
+                {{object.subtitle}}
+            </div>
+            <div v-if="object.demo" class="mb-3">
+                <ChessLesson 
+                    :header="object.lessonHeader"
+                    :demo="object.demo"
+                />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import MiddlegameStrategyText from '@/static/MiddlegameStrategyText';
+import ChessLesson from '@/components/subcomponents/ChessLesson';
 
 export default {
     name: 'MiddlegameStrategy',
+    components: {
+        ChessLesson,
+    },
     data() {
         return {
             MiddlegameStrategyText,

d8b0c4ba10aaaac4aa35f7c8506223c2ebe40d25
Byron Hall <byronhall@MacBook-Pro.local>
2021-02-04 19:02:36 -0600

    added OpeningStrategy Lessons

--- src/components/OpeningStrategy.vue
+++ src/components/OpeningStrategy.vue
@@ -10,15 +10,28 @@
             <div class="text-xl-h2 text-lg-h2 text-md-h3 text-sm-h4 text-h5 mb-3">
                 {{object.header}}
             </div>
+            <div v-if="object.subtitle" class="mb-3 text-justify">
+                {{object.subtitle}}
+            </div>
+            <div v-if="object.demo" class="mb-3">
+                <ChessLesson 
+                    :header="object.lessonHeader"
+                    :demo="object.demo"
+                />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import OpeningStrategyText from '@/static/OpeningStrategyText';
+import ChessLesson from '@/components/subcomponents/ChessLesson';
 
 export default {
     name: 'OpeningStrategy',
+    components: {
+        ChessLesson,
+    },
     data() {
         return {
             OpeningStrategyText,

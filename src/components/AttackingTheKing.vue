9cf7ff16b471218da08993b1077aea21186b6058
Byron Hall <byronhall@MacBook-Pro.local>
2021-02-06 17:12:42 -0600

    updated AttackingTheKing component

--- src/components/AttackingTheKing.vue
+++ src/components/AttackingTheKing.vue
@@ -1,42 +1,5 @@
 <template>
-    <div class="mt-3 mx-3">
-        <div class="text-xl-h1 text-lg-h1 text-md-h2 text-sm-h3 text-h4 mb-3">
-            {{AttackingTheKingText.header}}
-        </div>
-        <div class="text-body-1 mb-3 text-justify">
-            {{AttackingTheKingText.body}}
-        </div>
-        <div class="my-3" v-for="object in AttackingTheKingText.items" :key="object.header">
-            <div class="text-xl-h2 text-lg-h2 text-md-h3 text-sm-h4 text-h5 mb-3">
-                {{object.header}}
-            </div>
-            <div class="text-body-1 my-3">
-                {{object.body}}
-            </div>
-            <div v-for="item in object.buttons" :key="item.buttonText">
-                <v-dialog>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                            v-bind="attrs"
-                            v-on="on"
-                            block
-                            :class="$store.state.btnClass"
-                            outlined
-                        >
-                            <span :style="`color: ${$store.state.textColor}`">{{item.buttonText}}</span>
-                        </v-btn>
-                    </template>
-                    <v-list>
-                    <v-list-item>
-                        <v-list-item-title class="text-wrap">
-                            
-                        </v-list-item-title>
-                    </v-list-item>
-                    </v-list>
-                </v-dialog>
-            </div>
-        </div>
-    </div>
+    <div>Attacking The King</div>
 </template>
 
 <script>

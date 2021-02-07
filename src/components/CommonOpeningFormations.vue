2b33f91745dfa1fd921c33d6952a77f334151224
Byron Hall <byronhall@MacBook-Pro.local>
2021-02-07 09:14:22 -0600

    updated CommonOpeningFormations component

--- src/components/CommonOpeningFormations.vue
+++ src/components/CommonOpeningFormations.vue
@@ -1,27 +1,44 @@
 <template>
-    <div class="mt-3 mx-3">
-        <div class="text-xl-h1 text-lg-h1 text-md-h2 text-sm-h3 text-h4 mb-3">
-            {{CommonOpeningFormationsText.header}}
-        </div>
-        <div class="text-body-1 mb-3 text-justify">
-            {{CommonOpeningFormationsText.body}}
-        </div>
-        <div class="my-3" v-for="object in CommonOpeningFormationsText.items" :key="object.header">
-            <div class="text-xl-h2 text-lg-h2 text-md-h3 text-sm-h4 text-h5 mb-3">
-                {{object.header}}
-            </div>
-        </div>
+  <div class="mt-3">
+    <div class="text-xl-h1 text-lg-h1 text-md-h2 text-sm-h3 text-h4 mb-3">
+        {{CommonOpeningFormationsText.header}}
     </div>
+    <div class="text-body-1 mb-3 text-justify">
+        {{CommonOpeningFormationsText.body}}
+    </div>
+    <v-pagination
+        v-model="page"
+        :length="CommonOpeningFormationsText.items.length"
+        :color="`rgb(${$store.state.customColor.red},${$store.state.customColor.green},${$store.state.customColor.blue})`"
+    ></v-pagination>
+    <CustomHR />
+    <div class="my-3">
+      <div v-if="CommonOpeningFormationsText.items[page-1].demo" class="mb-3">
+        <ChessLesson 
+          :header="CommonOpeningFormationsText.items[page-1].lessonHeader"
+          :demo="CommonOpeningFormationsText.items[page-1].demo"
+        />
+      </div>
+    </div>
+    <CustomHR />
+  </div>
 </template>
 
 <script>
 import CommonOpeningFormationsText from '@/static/CommonOpeningFormationsText';
+import ChessLesson from '@/components/subcomponents/ChessLesson';
+import CustomHR from '@/components/subcomponents/CustomHR';
 
 export default {
     name: 'CommonOpeningFormations',
+    components: {
+        ChessLesson,
+        CustomHR,
+    },
     data() {
       return {
         CommonOpeningFormationsText,
+        page: 1,
       };
     },
 }
--- tests/unit/components/CommonOpeningFormations.spec.js
+++ tests/unit/components/CommonOpeningFormations.spec.js
@@ -1,9 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import CommonOpeningFormations from '@/components/CommonOpeningFormations.vue'
+import state from '@/store/State';
 
 describe('CommonOpeningFormations.vue', () => {
   it('renders component', () => {
-    const wrapper = shallowMount(CommonOpeningFormations);
+    const wrapper = shallowMount(CommonOpeningFormations, {
+      mocks: {
+        $store: {
+          state,
+        },
+    },
+    });
     expect(wrapper);
   })
 })

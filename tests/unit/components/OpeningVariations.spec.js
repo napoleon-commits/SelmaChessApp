55e95d6b12444ef62f45f32d90536add2b74707f
Byron Hall <byronhall@MacBook-Pro.local>
2021-02-07 03:47:31 -0600

    updated OpeningVariations Component

--- tests/unit/components/OpeningVariations.spec.js
+++ tests/unit/components/OpeningVariations.spec.js
@@ -1,9 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import OpeningVariations from '@/components/OpeningVariations.vue'
+import state from '@/store/State';
 
 describe('OpeningVariations.vue', () => {
   it('renders component', () => {
-    const wrapper = shallowMount(OpeningVariations);
+    const wrapper = shallowMount(OpeningVariations, {
+      mocks: {
+        $store: {
+          state,
+        },
+      },
+    });
     expect(wrapper);
   })
 })

b09ba4923c8f3575307d4629a2ed7220b23dc9f4
Byron Hall <byronhall@MacBook-Pro.local>
2021-02-07 02:32:08 -0600

    updated LandingPage

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Landing Page</div>
+  <div class="my-3 mx-3 row">
+      <div class="col d-flex justify-center">
+        <v-btn @click="goToOfflineChess" block outlined :class="$store.state.btnClass">
+            Offline Chess
+        </v-btn>
+      </div>
+      <div class="col d-flex justify-center">
+        <v-btn @click="goToTraining" block outlined :class="$store.state.btnClass">
+            Training
+        </v-btn>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
     name: 'LandingPage',
+    methods: {
+        goToOfflineChess(){
+            this.$router.push({name: 'OfflineChess'});
+        },
+        goToTraining(){
+            this.$router.push({name: 'Introduction'});
+        }
+    }
 }
 </script>
 

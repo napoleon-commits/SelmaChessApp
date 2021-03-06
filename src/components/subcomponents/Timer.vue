ee12b16bec78d4112f5bf715625db26596da9663
Byron Hall <byronhall@MacBook-Pro.local>
2021-03-05 23:14:05 -0600

    updated Timer format

--- src/components/subcomponents/Timer.vue
+++ src/components/subcomponents/Timer.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        {{String(hours).padStart(2,'0')}} : 
-        {{String(minutes).padStart(2,'0')}} .
+        {{String(minutes)}} :
         {{String(seconds).padStart(2,'0')}}
     </div>
 </template>
@@ -11,14 +10,11 @@ export default {
     name: 'Timer',
     props: ['totalSeconds'],
     computed: {
-        hours(){
-            return Math.floor(this.totalSeconds/3600);
-        },
         minutes(){
-            return Math.floor((this.totalSeconds-(this.hours*3600))/60);
+            return Math.floor((this.totalSeconds)/60);
         },
         seconds(){
-            return this.totalSeconds-(this.hours*3600)-(this.minutes*60);
+            return this.totalSeconds-(this.minutes*60);
         }
     },
 }

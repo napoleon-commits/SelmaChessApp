8e0df141873fd649b3bb705eeeb93f682cb4a8f5
Byron Hall <byronhall@MacBook-Pro.local>
2021-04-06 09:38:46 -0500

    added condition that analyzes bundle in development mode

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,16 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const plugins = [];
+if(process.env.NODE_ENV === 'development'){
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  plugins.push(
+    new BundleAnalyzerPlugin(),
+  );
+}
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
   "configureWebpack": {
     "devtool": "source-map",
-    "plugins": [
-      new BundleAnalyzerPlugin(),
-    ],
+    "plugins": plugins,
   }
 }
